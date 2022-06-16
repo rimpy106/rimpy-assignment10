4cbb9ff166aed2afca355e86cb92c7a443b10367
rimpy106 <95873438+rimpy106@users.noreply.github.com>
2022-06-15 19:17:05 -0500

    Update optional parameters

--- src/main/java/com/example/rimpyassignment10/web/SpoonacularController.java
+++ src/main/java/com/example/rimpyassignment10/web/SpoonacularController.java
@@ -1,11 +1,13 @@
 package com.example.rimpyassignment10.web;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -20,18 +22,21 @@ public class SpoonacularController {
 	private SpoonacularConfiguration config;
 
 	/*
-	 * @GetMapping("/mealplanner/week") public ResponseEntity<WeekResponse>
-	 * getWeekMeals(String numCalories, String diet, String exclusions) { return
-	 * getSpoonacularAPIWeekResponse(numCalories, diet, exclusions); }
-	 * 
-	 * @GetMapping("/mealplanner/day") public ResponseEntity<DayResponse>
-	 * getDayMeals(String numCalories, String diet, String exclusions) { return
-	 * getSpoonacularAPIDayResponse(numCalories, diet, exclusions); }
+	 * @GetMapping("/mealplanner/week") 
+	 * public ResponseEntity<WeekResponse> getWeekMeals(Optional<String> numCalories, Optional<String> diet,
+	 * Optional<String> exclusions) { String numCalValue = numCalories.isPresent() ?
+	 * numCalories.get() : null; String dietValue = diet.isPresent() ? diet.get() :
+	 * null; String exclusionsValue = exclusions.isPresent() ? exclusions.get() :
+	 * null; return getSpoonacularAPIWeekResponse(numCalValue, dietValue,
+	 * exclusionsValue); }
 	 */
-
+	
 	@GetMapping("/mealplanner/week")
-	public ResponseEntity<WeekResponse> getWeekMeals() {
-		return getSpoonacularAPIWeekResponse();
+	public ResponseEntity<WeekResponse> getWeekMeals(@RequestParam(required=false) Integer targetCalories, 
+			@RequestParam(required=false) String diet,
+			@RequestParam(required=false) String exclude) {
+		
+		return getSpoonacularAPIWeekResponse(targetCalories, diet, exclude);
 	}
 
 	@GetMapping("/mealplanner/day")
@@ -39,15 +44,16 @@ public class SpoonacularController {
 		return getSpoonacularAPIDayResponse();
 	}
 
-	public ResponseEntity<WeekResponse> getSpoonacularAPIWeekResponse() {
+	public ResponseEntity<WeekResponse> getSpoonacularAPIWeekResponse(Integer targetCalories, String diet,
+			String exclude) {
+		System.out.println(targetCalories + "-" + diet + "-" + exclude);
 		URI uri = UriComponentsBuilder.fromHttpUrl(config.getMealPlanUrl())
-				                      .queryParam("timeFrame", "week")
-				                      .queryParam("apiKey", "7a50a47ed75a4f5e9606e610628eb64f")
-				                      .build().toUri();
-				/*
-				 * .queryParam("targetCalories", Integer.parseInt(numCalories))
-				 * .queryParam("diet", diet) .queryParam("exclude", exclusions).
-				 */
+				.queryParam("timeFrame", "week")
+				.queryParam("apiKey", "7a50a47ed75a4f5e9606e610628eb64f")
+				.queryParam("targetCalories", targetCalories)
+				.queryParam("diet", diet)
+				.queryParam("exclude", exclude).build().toUri();
+
 		RestTemplate rt = new RestTemplate();
 		ResponseEntity<WeekResponse> weekResponse = rt.getForEntity(uri, WeekResponse.class);
 
@@ -57,9 +63,8 @@ public class SpoonacularController {
 	}
 
 	public ResponseEntity<DayResponse> getSpoonacularAPIDayResponse() {
-		URI uri = UriComponentsBuilder.fromHttpUrl(config.getMealPlanUrl())
-				                      .queryParam("timeFrame", "day")
-				                      .queryParam("apiKey", "7a50a47ed75a4f5e9606e610628eb64f").build().toUri();
+		URI uri = UriComponentsBuilder.fromHttpUrl(config.getMealPlanUrl()).queryParam("timeFrame", "day")
+				.queryParam("apiKey", "7a50a47ed75a4f5e9606e610628eb64f").build().toUri();
 
 		RestTemplate rt1 = new RestTemplate();
 		ResponseEntity<DayResponse> dayResponse = rt1.getForEntity(uri, DayResponse.class);
