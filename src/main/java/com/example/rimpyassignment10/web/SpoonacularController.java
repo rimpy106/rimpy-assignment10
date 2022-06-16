c3a93c5e6a50638a6f71e9b766cae31334636ea2
rimpy106 <95873438+rimpy106@users.noreply.github.com>
2022-06-16 14:41:46 -0500

    Update optional parameters

--- src/main/java/com/example/rimpyassignment10/web/SpoonacularController.java
+++ src/main/java/com/example/rimpyassignment10/web/SpoonacularController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -22,44 +21,42 @@ public class SpoonacularController {
 	private SpoonacularConfiguration config;
 	
 	@GetMapping("/mealplanner/week")
-	public ResponseEntity<WeekResponse> getWeekMeals(Optional<Integer> numCalories, Optional<String> diet,
-			Optional<String> exclusions) {
+	public ResponseEntity<WeekResponse> getWeekMeals(Optional<Integer> targetCalories, Optional<String> diet,
+			Optional<String> exclude) {
 		
 		RestTemplate rt = new RestTemplate();
 		
 		URI uri = UriComponentsBuilder.fromHttpUrl(config.getMealPlanUrl())
 				.queryParam("timeFrame", "week")
 				.queryParam("apiKey", "7a50a47ed75a4f5e9606e610628eb64f")
-				.queryParamIfPresent("targetCalories", numCalories) 
+				.queryParamIfPresent("targetCalories", targetCalories) 
 				.queryParamIfPresent("diet", diet)
-				.queryParamIfPresent("exclude", exclusions).build().toUri();
+				.queryParamIfPresent("exclude", exclude).build().toUri();
 
 		
 		ResponseEntity<WeekResponse> weekResponse = rt.getForEntity(uri, WeekResponse.class);
 
 		System.out.println(weekResponse.getBody());
-		return weekResponse;
-		
-
+		return weekResponse;	
 	}
 
 	@GetMapping("/mealplanner/day")
-	public ResponseEntity<DayResponse> getDayMeals(Optional<Integer> numCalories, Optional<String> diet,
-			Optional<String> exclusions) {
+	public ResponseEntity<DayResponse> getDayMeals(Optional<Integer> targetCalories, Optional<String> diet,
+			Optional<String> exclude) {
 		
 		RestTemplate rt1 = new RestTemplate();
 		
 		URI uri = UriComponentsBuilder.fromHttpUrl(config.getMealPlanUrl())
 				                      .queryParam("timeFrame", "day")
 				                      .queryParam("apiKey", "7a50a47ed75a4f5e9606e610628eb64f")
-				                  	  .queryParamIfPresent("targetCalories", numCalories) 
+				                  	  .queryParamIfPresent("targetCalories", targetCalories) 
 				    				  .queryParamIfPresent("diet", diet)
-				    				  .queryParamIfPresent("exclude", exclusions).build().toUri();
+				    				  .queryParamIfPresent("exclude", exclude).build().toUri();
 
 		
 		ResponseEntity<DayResponse> dayResponse = rt1.getForEntity(uri, DayResponse.class);
 
-		//System.out.println(dayResponse.getBody());
+		System.out.println(dayResponse.getBody());
 		return dayResponse;
 	}
 
